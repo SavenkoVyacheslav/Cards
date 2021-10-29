cc082990f8c632c60bbe59b92e17666ed214b96f
SavenkoVyacheslav <76222126+SavenkoVyacheslav@users.noreply.github.com>
2021-10-29 13:56:37 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function createCard(contact) {
+  return (
+    <Card
+      id={contact.id}
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="//media-exp1.licdn.com/dms/image/C4E03AQF4Jq8xpiHxRQ/profile-displayphoto-shrink_400_400/0/1630615974836?e=1640822400&v=beta&t=cR0_h7waNV3lHRuPD8eBVYFYxMlaiWFeTbHIywXr8lI" />
+      {contacts.map(createCard)}
+      {/* <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      /> */}
+    </div>
+  );
+}
+
+export default App;
